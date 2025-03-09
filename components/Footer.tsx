a574946410ea47f1076d5f66474db00e30c48da6
Nakamury1 <sophielamsova29@gmail.com>
2025-03-09 23:13:28 +0100

    fix: footer colors

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -4,7 +4,7 @@ import "../public/style.css"
 
 export default function Footer(){
     return(
-        <footer className="w-full bg-purple-700 p-8">
+        <footer className="w-full bg-black p-8">
             <p className="text-purple-100 text-lg flex justify-center">Copyright: Sophie LAM</p>
         </footer>
     );
